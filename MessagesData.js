1f7f2862b137d192a565bf9af9146a42bae09e10
John <tamecalm@protonmail.com>
2024-12-14 01:41:54 +0100

    Update MessagesData.js

--- MessagesData.js
+++ MessagesData.js
@@ -19,21 +19,82 @@ const MessagesData = [
         message: "You're Cuteee 😫😍!",
         theme: "--grass"
     },
-    
     {
         message: "You can overcome it 😋!",
         theme: "--orchin"
     },
-    
     {
         message: "You and Sapa will be besties 🤣!",
         theme: "--thanos"
     },
-    
     {
         message: "God loves you 😘",
         theme: "--gold"
+    },
+    {
+        message: "You're doing great! 🌟",
+        theme: "--aqua"
+    },
+    {
+        message: "The world needs you 🌍",
+        theme: "--sky"
+    },
+    {
+        message: "You're a star 🌟✨",
+        theme: "--galaxy"
+    },
+    {
+        message: "Keep going, you've got this 💪",
+        theme: "--fire"
+    },
+    {
+        message: "You're a rare gem 💎",
+        theme: "--sapphire"
+    },
+    {
+        message: "You light up the room 🌈",
+        theme: "--sunbeam"
+    },
+    {
+        message: "Your smile is contagious 😊",
+        theme: "--daisy"
+    },
+    {
+        message: "You're one of a kind 🌟",
+        theme: "--twilight"
+    },
+    {
+        message: "Success looks good on you 🏆",
+        theme: "--emerald"
+    },
+    {
+        message: "Take a deep breath, you're amazing 🌬️",
+        theme: "--breeze"
+    },
+    {
+        message: "Every step forward counts 🚶‍♂️🚶‍♀️",
+        theme: "--earth"
+    },
+    {
+        message: "Your kindness is your superpower 🦸",
+        theme: "--lavender"
+    },
+    {
+        message: "You're a dreamer and a doer 🌟",
+        theme: "--cosmic"
+    },
+    {
+        message: "You're a force to be reckoned with 🌪️",
+        theme: "--storm"
+    },
+    {
+        message: "Keep shining like the diamond you are 💎",
+        theme: "--crystal"
+    },
+    {
+        message: "You've got the heart of a lion 🦁",
+        theme: "--courage"
     }
-]
+];
 
-export default MessagesData
+export default MessagesData;
