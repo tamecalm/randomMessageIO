9e451758c242e4b879bfa77f8243b3773fb64e34
John <tamecalm@protonmail.com>
2025-01-21 16:44:50 +0100

    Update MessagesData.js

--- MessagesData.js
+++ MessagesData.js
@@ -19,21 +19,74 @@ const MessagesData = [
         message: "You're Cuteee 😫😍!",
         theme: "--grass"
     },
-    
     {
         message: "You can overcome it 😋!",
         theme: "--orchin"
     },
-    
     {
         message: "You and Sapa will be besties 🤣!",
         theme: "--thanos"
     },
-    
     {
         message: "God loves you 😘",
         theme: "--gold"
+    },
+    {
+        message: "You’re stronger than you think 💪",
+        theme: "--strength"
+    },
+    {
+        message: "Keep shining, the world needs your light 🌟",
+        theme: "--light"
+    },
+    {
+        message: "Your potential is limitless 🚀",
+        theme: "--universe"
+    },
+    {
+        message: "Dream big, you’ve got this! 🌈",
+        theme: "--sky"
+    },
+    {
+        message: "Every step you take matters! 🦶",
+        theme: "--path"
+    },
+    {
+        message: "You’re a masterpiece in the making 🎨",
+        theme: "--canvas"
+    },
+    {
+        message: "Everything will fall into place 👏",
+        theme: "--balance"
+    },
+    {
+        message: "You are a blessing to the world 🌍",
+        theme: "--bless"
+    },
+    {
+        message: "Keep going, your efforts will pay off! 💥",
+        theme: "--action"
+    },
+    {
+        message: "Your smile brightens up the day! 😁",
+        theme: "--smile"
+    },
+    {
+        message: "Good things are coming your way! 🌠",
+        theme: "--fortune"
+    },
+    {
+        message: "You’ve got the power to change the world 🌎",
+        theme: "--power"
+    },
+    {
+        message: "You’re destined for greatness 🌟",
+        theme: "--destiny"
+    },
+    {
+        message: "The best is yet to come ✨",
+        theme: "--future"
     }
-]
+];
 
-export default MessagesData
+export default MessagesData;
